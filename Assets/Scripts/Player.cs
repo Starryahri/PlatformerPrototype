250a36ddd1f9dc12da87e10ca62d168c112f505e
Ahria Nicholas <ahria.nicholas@wsu.edu>
2021-02-14 19:31:23 -0800

    early prototype is complete

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,9 +7,17 @@ public class Player : MonoBehaviour
     private CharacterController _controller;
 
     [SerializeField]
-    private float _playerSpeed = 5.0f;
-    private float _jumpHeight = 2f;
-    private float _gravityVal = -9.81f;
+    private float _playerSpeed = 10.0f;
+    [SerializeField]
+    private float _jumpHeight = 25.0f;
+    [SerializeField]
+    private float _dbJumpHeight = 23.0f;
+    [SerializeField]
+    private float _gravityValue = -1.81f;
+    private float _yVelocity = 0;
+
+    private bool _canDoubleJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,8 +29,37 @@ public class Player : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
 
-        Vector2 direction = new Vector2(horizontalInput, 0, 0);
-        Vector2 velocity = direction * _playerSpeed;        
-        _controller.Move(direction * _playerSpeed * Time.deltaTime);
+        Vector2 direction = new Vector2(horizontalInput, 0);
+        Vector2 velocity = direction * _playerSpeed;
+
+        if(_controller.isGrounded == true)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _canDoubleJump = true;
+            }
+        }
+        else
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_canDoubleJump == true)
+                {
+                    _yVelocity += _dbJumpHeight;
+                    _canDoubleJump = false;
+                }
+
+            }
+            _yVelocity += _gravityValue;
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+
+        if(transform.position.y < -7.5f)
+        {
+            transform.position = new Vector3(-11.12f, 3.84f, -4f);
+        }
     }
 }
